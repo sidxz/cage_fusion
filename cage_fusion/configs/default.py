1b4a3ae871c22044fad45acb7a747b2b778ea873
Siddhant Rath <sid@tamu.edu>
2025-07-05 11:50:02 -0500

    v1.2-proto

--- cage_fusion/configs/default.py
+++ cage_fusion/configs/default.py
@@ -24,12 +24,12 @@ def get_default_config():
         "embedding_dim": 768,  # Token embedding size
         "aux_feature_dim": 217,  # Dimension of auxiliary features
         "num_tasks": 4,  # Number of prediction tasks
-        "num_heads": 4,  # Number of attention heads
+        "num_heads": 8,  # Number of attention heads
         "cross_attn_dropout": 0.1,  # Dropout rate in cross-attention
         "proj_dropout": 0.1,  # Dropout rate in projection layers
         "graph_only_mode": False,  # Use only graph features without token embeddings
         "use_co_attention": True,  # Use Co-Attention mechanism
-        "use_aux_features": False,  # Use auxiliary features in the model
+        "use_aux_features": True,  # Use auxiliary features in the model
         "co_attention_layers": 2,  # Number of Co-Attention layers
         "fusion_dropout_1": 0.2,  # Dropout rate in first fusion layer
         "fusion_dropout_2": 0.1,  # Dropout rate in second
@@ -40,7 +40,7 @@ def get_default_config():
         "warmup_fraction": 0.2,  # Warmup steps as a fraction of total steps
         "clip_grad_norm": 1.0,  # Gradient clipping to prevent exploding gradients
         # Scaled Attention Factor
-        "scaled_graph_factor": 1.0,  # Scaling factor for attention scores
+        "scaled_graph_factor": 18.0,  # Scaling factor for attention scores
         "scale_attn_factor": 1.0,  # Scaling factor for attention scores
         "scale_aux_factor": 0.01,  # Scaling factor for auxiliary features
         # === Attention Regularization ===
