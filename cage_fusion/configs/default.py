111db4a257a634e92652e7e578ae28d8469930e7
Siddhant Rath <sid@tamu.edu>
2025-12-02 12:00:49 -0600

    other env

--- cage_fusion/configs/default.py
+++ cage_fusion/configs/default.py
@@ -22,13 +22,13 @@ def get_default_config():
         # === Model Hyperparameters ===
         "attn_mode": "self_graph",  # Attention mode: 'cross', 'self_tokens', 'self_graph' or 'self_both'
         "graph_dim": 300,  # Dimension of graph features
-        "embedding_dim": 768,  # Token embedding size -> unikei/bert-base-smiles
-        #"embedding_dim": 384,  # Token embedding size -> DeepChem/ChemBERTa-77M-MTR
+        #"embedding_dim": 768,  # Token embedding size -> unikei/bert-base-smiles
+        "embedding_dim": 384,  # Token embedding size -> DeepChem/ChemBERTa-77M-MTR
         "aux_feature_dim": 217,  # Dimension of auxiliary features
         "num_tasks": 4,  # Number of prediction tasks
         "num_heads": 8,  # Number of attention heads
-        "cross_attn_dropout": 0.01,  # Dropout rate in cross-attention
-        "proj_dropout": 0.06,  # Dropout rate in projection layers
+        "cross_attn_dropout": 0.15,  # Dropout rate in cross-attention
+        "proj_dropout": 0.10,  # Dropout rate in projection layers
         # === Model Architecture Settings ===
         "use_co_attention": True,  # Use Co-Attention mechanism
         "use_aux_features": True,  # Use auxiliary features in the model
@@ -40,8 +40,8 @@ def get_default_config():
         "scale_aux_factor": 0.5,  # Scaling factor for auxiliary features
         "scaled_fg_factor": 1.0,  # Scaling factor for functional group prompts
         # === Fusion Layer Settings ===
-        "fusion_dropout_1": 0.2,  # Dropout rate in first fusion layer
-        "fusion_dropout_2": 0.3,  # Dropout rate in second
+        "fusion_dropout_1": 0.3,  # Dropout rate in first fusion layer
+        "fusion_dropout_2": 0.2,  # Dropout rate in second
         # === Training Hyperparameters ===
         "learning_rate": 0.001,  # Learning rate for the optimizer
         "num_epochs": 50,  # Total number of training epochs
@@ -52,8 +52,8 @@ def get_default_config():
         "lambda_prior": 0.000,  # Prior loss weight using token importance
         # === Data Processing Settings ===
         # === Tokenizer & Model Checkpoint ===
-        "model_checkpoint": "unikei/bert-base-smiles",  # Pretrained tokenizer/model checkpoint
-        #"model_checkpoint": "DeepChem/ChemBERTa-77M-MTR",  # Pretrained tokenizer/model checkpoint
+        #"model_checkpoint": "unikei/bert-base-smiles",  # Pretrained tokenizer/model checkpoint
+        "model_checkpoint": "DeepChem/ChemBERTa-77M-MTR",  # Pretrained tokenizer/model checkpoint
         # === Execution Device ===
         "device": device_type,  # Automatically assigned device based on CUDA availability
         # === Optional Settings ===
